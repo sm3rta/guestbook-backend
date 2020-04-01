21f6af6b3fc95936dc4cbe43b4b54bff17559d59
Ahmed <ahmed@hayasolutions.com>
2020-04-02 00:34:37 +0200

    added bodyParser middleware to the app

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+//use body parser to parse request body
+const bodyParser = require("body-parser");
+app.use(bodyParser.json({ type: "*/*" }));
+
+//import subroutes
 const usersRoute = require("./routes/users");
 
+//add subroutes to the application
 app.use("/users", usersRoute);
 
 const port = process.env.PORT || 3001;
