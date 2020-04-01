4c38079b47fadd43cbeaae61d92043a4b3fb80c9
Ahmed <ahmed@hayasolutions.com>
2020-04-02 01:00:39 +0200

    registered signin route in users router

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const signupRoute = require("./signup");
+const signinRoute = require("./signin");
 
 router.use("/signup", signupRoute);
+router.use("/signin", signinRoute);
 module.exports = router;
