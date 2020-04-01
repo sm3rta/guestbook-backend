9c91109ceb913ae9adb6c95581b01b013adb476b
Ahmed <ahmed@hayasolutions.com>
2020-04-02 00:35:11 +0200

    added validation to signup route

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,13 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("@hapi/joi");
 const User = require("../../schemas/user");
+const hashPassword = require("../../utils/hashPassword");
 
-router.get("/signup", async (req, res) => {
-  const { name, username, password } = req.body;
+const checkUserExists = async email => {
+  const user = await User.findOne({ email });
+  return !!user;
+};
 
-  const user = new User({ name, username });
+const userSchema = Joi.object({
+  email: Joi.string().email(),
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+});
+
+router.post("/signup", async (req, res) => {
+  const validation = userSchema.validate(req.body);
+  if (validation.error) {
+    return res
+      .status(400)
+      .send({ error: true, message: validation.error.message });
+  }
+
+  const { name, email, password } = req.body;
+
+  //check if user alreeady exists
+  const userExists = await checkUserExists(email);
+
+  if (userExists)
+    return res
+      .status(409)
+      .send({ error: true, message: "User already exists" });
+
+  //all is well, create user document and add it to the database
+  const user = new User({ name, email, password: hashPassword(password) });
   const result = await user.save();
-  res.status(200).send({ message: "User added successfully" });
+  res.status(200).send({ error: false, message: "User added successfully" });
 });
 
 module.exports = router;
