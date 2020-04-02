9da68b32de3c97fe91955b6ee4366a97af7d21e5
Ahmed <ahmed@hayasolutions.com>
2020-04-02 04:29:03 +0200

    added a route to add replies to database

---
+++ routes/replies/addReply.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("@hapi/joi");
+const Message = require("../../schemas/message");
+const Reply = require("../../schemas/reply");
+
+const replySchema = Joi.object({
+  content: Joi.string()
+    .min(3)
+    .max(500)
+    .required(),
+  submittedBy: Joi.string().required(),
+  messageId: Joi.string().required(),
+  date: Joi.date()
+});
+
+router.post("/", async (req, res) => {
+  const validation = replySchema.validate(req.body);
+  if (validation.error) {
+    return res
+      .status(400)
+      .send({ error: true, message: validation.error.message });
+  }
+
+  const { content, submittedBy, messageId } = req.body;
+  const reply = new Reply({
+    content,
+    submittedBy
+  });
+  const savedReply = await reply.save();
+  console.log("savedReply", savedReply);
+  const message = await Message.findById(messageId);
+  message.replies.push(savedReply._id);
+  const savedMessage = await message.save();
+  console.log("savedMessage", savedMessage);
+  res.status(200).send({ error: false, message: "Message added successfully" });
+});
+
+module.exports = router;
--- routes/replies/index.js
+++ routes/replies/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+const addReplyRoute = require("./addReply");
+router.use("/", addReplyRoute);
+
 module.exports = router;
