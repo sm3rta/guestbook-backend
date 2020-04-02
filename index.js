ed895ee945acdeb49d4e1521e06239e72c20664d
Ahmed <ahmed@hayasolutions.com>
2020-04-02 02:06:11 +0200

    registered messages route in the main app

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ app.use(bodyParser.json({ type: "*/*" }));
 
 //import subroutes
 const usersRoute = require("./routes/users");
-
+const messagesRoute = require("./routes/messages");
 //add subroutes to the application
 app.use("/users", usersRoute);
+app.use("/messages", messagesRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
