8066a31a353abcfb646ebf4637c53fe64c76214e
Ahmed <ahmed@hayasolutions.com>
2020-04-02 04:29:37 +0200

    get messages route now returns populated data instead of object refs

--- routes/messages/getMessages.js
+++ routes/messages/getMessages.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const Message = require("../../schemas/message");
 
 router.get("/", async (req, res) => {
-  const messages = await Message.find();
+  const messages = await Message.find()
+    .populate("submittedBy")
+    .populate("replies");
+
   res.status(200).send({ error: false, data: { messages } });
 });
 
