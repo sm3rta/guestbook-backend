8a9bc9d94ef33b902ae006802d12514cdf2258ad
Ahmed <ahmed@hayasolutions.com>
2020-04-02 03:05:53 +0200

    added a router for replies

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ app.use(bodyParser.json({ type: "*/*" }));
 //import subroutes
 const usersRoute = require("./routes/users");
 const messagesRoute = require("./routes/messages");
+const repliesRoute = require("./routes/replies");
 //add subroutes to the application
 app.use("/users", usersRoute);
 app.use("/messages", messagesRoute);
+app.use("/replies", repliesRoute);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
