dda79fd4e943e6abe71ba550d0034efe4026279f
Ahmed <ahmed@hayasolutions.com>
2020-04-03 22:52:08 +0200

    message deletion now deletes all associated replies

--- routes/messages/deleteMessage.js
+++ routes/messages/deleteMessage.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const Message = require("../../schemas/message");
+const Reply = require("../../schemas/reply");
 
 router.delete("/:messageId", async (req, res) => {
   const messageId = req.params.messageId;
 
   const messageDeletionResult = await Message.findByIdAndDelete(messageId);
+  messageDeletionResult.replies.forEach(replyId =>
+    Reply.findByIdAndDelete(replyId)
+  );
   console.log("messageDeletionResult", messageDeletionResult);
 
   if (messageDeletionResult) {
