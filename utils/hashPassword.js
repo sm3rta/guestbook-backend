15ae9216d37a8113726539c676e5e623f44fc2f6
Ahmed <ahmed@hayasolutions.com>
2020-04-04 16:57:00 +0200

    documented hashPassword method

--- utils/hashPassword.js
+++ utils/hashPassword.js
@@ -1,6 +1,15 @@
 const crypto = require("crypto");
 
-const hashPassword = password => {
+/**
+ * @method
+ * @description hashes passwords
+ * @param {string} password
+ * @returns hashed password
+ * @example
+ * // returns sEfaPnCDyMXeulzW/ByRk54mBMeejuqc4IAyiwFNYqU=
+ * hashPassword("ABCD@123");
+ */
+const hashPassword = (password) => {
   const sha256 = crypto.createHash("sha256");
   const hash = sha256.update(password).digest("base64");
   return hash;
