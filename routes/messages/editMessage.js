db637368e410f3a42dc6533c1182fb32d726154c
Ahmed <ahmed@hayasolutions.com>
2020-04-04 17:16:42 +0200

    documented edit messages route handler

--- routes/messages/editMessage.js
+++ routes/messages/editMessage.js
@@ -1,41 +1,52 @@
+/**
+ * @module
+ * @description adds the edit Message route handler
+ */
 const express = require("express");
 const router = express.Router();
 const Joi = require("@hapi/joi");
 const Message = require("../../schemas/message");
 
 const messageSchema = Joi.object({
-  content: Joi.string()
-    .min(3)
-    .max(500)
-    .required()
+  content: Joi.string().min(3).max(500).required(),
 });
 
-router.patch("/:messageId", async (req, res) => {
-  const validation = messageSchema.validate(req.body);
-  if (validation.error) {
-    return res
-      .status(400)
-      .send({ error: true, message: validation.error.message });
+router.patch(
+  "/:messageId",
+  /**
+   * @callback addMessageRequestHandler
+   * @description validates request data and sends
+   * - 400 if data is invalid
+   * - 200 if data is correct and the message is edited successfully
+   */
+
+  async (req, res) => {
+    const validation = messageSchema.validate(req.body);
+    if (validation.error) {
+      return res
+        .status(400)
+        .send({ error: true, message: validation.error.message });
+    }
+
+    const messageId = req.params.messageId;
+    const { content } = req.body;
+
+    const editedMessage = await Message.findByIdAndUpdate(
+      messageId,
+      {
+        $set: {
+          content,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(200).send({
+      error: false,
+      message: "Message edited successfully",
+      data: { editedMessage },
+    });
   }
-
-  const messageId = req.params.messageId;
-  const { content } = req.body;
-
-  const editedMessage = await Message.findByIdAndUpdate(
-    messageId,
-    {
-      $set: {
-        content
-      }
-    },
-    { new: true }
-  );
-
-  res.status(200).send({
-    error: false,
-    message: "Message edited successfully",
-    data: { editedMessage }
-  });
-});
+);
 
 module.exports = router;
