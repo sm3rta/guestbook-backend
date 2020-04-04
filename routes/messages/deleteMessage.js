07caa581a42a9a2b7f5c12ab569ccef59dae1aa7
Ahmed <ahmed@hayasolutions.com>
2020-04-04 17:16:49 +0200

    documented delete messages route handler

--- routes/messages/deleteMessage.js
+++ routes/messages/deleteMessage.js
@@ -1,28 +1,41 @@
+/**
+ * @module
+ * @description adds the delete Message route handler
+ */
 const express = require("express");
 const router = express.Router();
 const Message = require("../../schemas/message");
 const Reply = require("../../schemas/reply");
 
-router.delete("/:messageId", async (req, res) => {
-  const messageId = req.params.messageId;
+router.delete(
+  "/:messageId",
+  /**
+   * @callback deleteMessageRequestHandler
+   * @description sends
+   * - 400 if the message doesn't exist
+   * - 200 if the message is deleted successfully
+   */
+  async (req, res) => {
+    const messageId = req.params.messageId;
 
-  const messageDeletionResult = await Message.findByIdAndDelete(messageId);
-  messageDeletionResult.replies.forEach(replyId =>
-    Reply.findByIdAndDelete(replyId)
-  );
-  console.log("messageDeletionResult", messageDeletionResult);
+    const messageDeletionResult = await Message.findByIdAndDelete(messageId);
+    messageDeletionResult.replies.forEach((replyId) =>
+      Reply.findByIdAndDelete(replyId)
+    );
+    console.log("messageDeletionResult", messageDeletionResult);
 
-  if (messageDeletionResult) {
-    res.status(200).send({
-      error: false,
-      message: "Message deleted successfully"
-    });
-  } else {
-    res.status(400).send({
-      error: true,
-      message: "Message to be deleted doesn't exist"
-    });
+    if (messageDeletionResult) {
+      res.status(200).send({
+        error: false,
+        message: "Message deleted successfully",
+      });
+    } else {
+      res.status(400).send({
+        error: true,
+        message: "Message to be deleted doesn't exist",
+      });
+    }
   }
-});
+);
 
 module.exports = router;
