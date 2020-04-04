9526802ac81d30bfebaa98e13ac6d6eb56d9080c
Ahmed <ahmed@hayasolutions.com>
2020-04-04 17:08:12 +0200

    documented add reply route handler

--- routes/replies/addReply.js
+++ routes/replies/addReply.js
@@ -1,3 +1,7 @@
+/**
+ * @module
+ * @description adds the add reply route handler
+ */
 const express = require("express");
 const router = express.Router();
 const Joi = require("@hapi/joi");
@@ -5,35 +9,41 @@ const Message = require("../../schemas/message");
 const Reply = require("../../schemas/reply");
 
 const replySchema = Joi.object({
-  content: Joi.string()
-    .min(3)
-    .max(500)
-    .required(),
+  content: Joi.string().min(3).max(500).required(),
   submittedBy: Joi.string().required(),
   messageId: Joi.string().required(),
-  date: Joi.date()
+  date: Joi.date(),
 });
 
-router.post("/", async (req, res) => {
-  const validation = replySchema.validate(req.body);
-  if (validation.error) {
-    return res
-      .status(400)
-      .send({ error: true, message: validation.error.message });
+router.post(
+  "/",
+  /**
+   * @callback addReplyRequestHandler
+   * @description validates request data and sends
+   * - 400 if data is invalid
+   * - 200 if data is correct and the reply object is added successfully
+   */
+  async (req, res) => {
+    const validation = replySchema.validate(req.body);
+    if (validation.error) {
+      return res
+        .status(400)
+        .send({ error: true, message: validation.error.message });
+    }
+
+    const { content, submittedBy, messageId } = req.body;
+    const reply = new Reply({
+      content,
+      submittedBy,
+    });
+    const savedReply = await reply.save();
+    console.log("savedReply", savedReply);
+    const message = await Message.findById(messageId);
+    message.replies.push(savedReply._id);
+    const savedMessage = await message.save();
+    console.log("savedMessage", savedMessage);
+    res.status(200).send({ error: false, message: "Reply added successfully" });
   }
-
-  const { content, submittedBy, messageId } = req.body;
-  const reply = new Reply({
-    content,
-    submittedBy
-  });
-  const savedReply = await reply.save();
-  console.log("savedReply", savedReply);
-  const message = await Message.findById(messageId);
-  message.replies.push(savedReply._id);
-  const savedMessage = await message.save();
-  console.log("savedMessage", savedMessage);
-  res.status(200).send({ error: false, message: "Reply added successfully" });
-});
+);
 
 module.exports = router;
