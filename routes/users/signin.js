8accec1a60eda49d25dad8a391be165e35c79561
Ahmed <ahmed@hayasolutions.com>
2020-04-04 17:07:51 +0200

    documented signin route handler

--- routes/users/signin.js
+++ routes/users/signin.js
@@ -1,40 +1,62 @@
+/**
+ * @module
+ * @description adds the signin route handler
+ */
 const express = require("express");
 const router = express.Router();
 const Joi = require("@hapi/joi");
 const User = require("../../schemas/user");
 const hashPassword = require("../../utils/hashPassword");
 
+/**
+ * @method
+ * @description checks if a user with given credentials exist in the database
+ * @param {Object} credentials an object containing email and password
+ * @param {string} credentials.email email
+ * @param {string} credentials.password password
+ * @returns
+ * - if user exists, returns user object.
+ * - if not, returns null
+ */
 const checkUserCredentials = async ({ email, password }) => {
   const user = await User.findOne({ email, password: hashPassword(password) });
   return user;
 };
 
 const userSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required(),
-  password: Joi.string().required()
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
 });
 
-router.post("/", async (req, res) => {
-  const validation = userSchema.validate(req.body);
-  if (validation.error) {
-    return res
-      .status(400)
-      .send({ error: true, message: validation.error.message });
-  }
+router.post(
+  "/",
+  /**
+   * @callback signinRequestHandler
+   * @description validates request data and sends
+   * - 400 if data is invalid
+   * - 401 wrong id or password
+   * - 200 along with user object if data is correct and user does exist
+   */
+  async (req, res) => {
+    const validation = userSchema.validate(req.body);
+    if (validation.error) {
+      return res
+        .status(400)
+        .send({ error: true, message: validation.error.message });
+    }
 
-  //check if user alreeady exists
-  const user = await checkUserCredentials(req.body);
+    //check if user alreeady exists
+    const user = await checkUserCredentials(req.body);
 
-  if (user) {
-    user.password = undefined;
-    res
-      .status(200)
-      .send({ error: false, message: "User login successful", data: user });
-  } else {
-    res.status(401).send({ error: true, message: "Wrong email or password" });
+    if (user) {
+      user.password = undefined;
+      res
+        .status(200)
+        .send({ error: false, message: "User login successful", data: user });
+    } else {
+      res.status(401).send({ error: true, message: "Wrong email or password" });
+    }
   }
-});
+);
 
 module.exports = router;
