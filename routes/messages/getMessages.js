f4c82cd702f6ed00f9151a848f777350a476735a
Ahmed <ahmed@hayasolutions.com>
2020-04-04 17:16:33 +0200

    documented get messages route handler

--- routes/messages/getMessages.js
+++ routes/messages/getMessages.js
@@ -1,17 +1,28 @@
+/**
+ * @module
+ * @description adds the get Messages route handler
+ */
 const express = require("express");
 const router = express.Router();
 const Message = require("../../schemas/message");
 
-router.get("/", async (req, res) => {
-  const messages = await Message.find()
-    .populate("submittedBy", ["_id", "name", "email"])
-    .populate({
-      path: "replies",
-      populate: { path: "submittedBy", select: ["_id", "name", "email"] }
-    })
-    .sort({ date: -1 });
+router.get(
+  "/",
+  /**
+   * @callback getMessagesRequestHandler
+   * @description sends all the messages with a 200 status code
+   */
+  async (req, res) => {
+    const messages = await Message.find()
+      .populate("submittedBy", ["_id", "name", "email"])
+      .populate({
+        path: "replies",
+        populate: { path: "submittedBy", select: ["_id", "name", "email"] },
+      })
+      .sort({ date: -1 });
 
-  res.status(200).send({ error: false, data: { messages } });
-});
+    res.status(200).send({ error: false, data: { messages } });
+  }
+);
 
 module.exports = router;
