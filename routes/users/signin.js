51c5432d73646bce3adb18c187bac2a724d48bd2
Ahmed <ahmed@hayasolutions.com>
2020-04-04 03:14:02 +0200

    removed unnecessary regex from password in signin

--- routes/users/signin.js
+++ routes/users/signin.js
@@ -13,9 +13,7 @@ const userSchema = Joi.object({
   email: Joi.string()
     .email()
     .required(),
-  password: Joi.string()
-    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-    .required()
+  password: Joi.string().required()
 });
 
 router.post("/", async (req, res) => {
