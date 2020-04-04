20a56d222ea93dafcb990b1af6d2cd2f345e7da6
Ahmed <ahmed@hayasolutions.com>
2020-04-04 16:57:51 +0200

    documented reply schema model

--- schemas/reply.js
+++ schemas/reply.js
@@ -7,12 +7,24 @@ const { Schema } = mongoose;
 const replySchema = new Schema({
   submittedBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   content: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
 
+/**
+ * @class
+ * @classdesc database model for Reply\
+ * a Reply object references a User in the submittedBy
+ * and is referenced by a Message
+ * @description creates a mongoDB reply object
+ * @param {Object} replyData reply info to store in database
+ * @param {string} replyData.content reply content
+ * @param {string} replyData.submittedBy ref to User object,
+ * the user who submitted the reply
+ * @param {Date} replyData.date reply submission date
+ */
 const Reply = mongoose.model("Reply", replySchema);
 
 module.exports = Reply;
