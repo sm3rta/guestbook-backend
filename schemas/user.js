2e48ecacbf705bb183d0cd4157f265a7ca67cd5e
Ahmed <ahmed@hayasolutions.com>
2020-04-04 16:57:42 +0200

    documented user schema model

--- schemas/user.js
+++ schemas/user.js
@@ -6,9 +6,18 @@ connectToDataBase();
 const userSchema = new mongoose.Schema({
   email: String,
   name: String,
-  password: String
+  password: String,
 });
 
+/**
+ * @class
+ * @classdesc database model for User
+ * @description creates a mongoDB user object
+ * @param {Object} userData user info to store in database
+ * @param {string} userData.name user's name
+ * @param {string} userData.email user email
+ * @param {string} userData.password user password hashed
+ */
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
