9e67a3cb794d1d3401380595b6941c5b1cd13e5d
Ahmed <ahmed@hayasolutions.com>
2020-04-05 02:37:50 +0200

    removed getMessages from messages router and used middlerware

--- routes/messages/index.js
+++ routes/messages/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const addMessageRoute = require("./addMessage");
 const editMessageRoute = require("./editMessage");
 const deleteMessageRoute = require("./deleteMessage");
-const getMessagesRoute = require("./getMessages");
+const authMiddleware = require("../../middleware/auth");
 
+router.use(authMiddleware);
 router.use("/", addMessageRoute);
 router.use("/", editMessageRoute);
 router.use("/", deleteMessageRoute);
-router.use("/", getMessagesRoute);
 
 module.exports = router;
